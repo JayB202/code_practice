c5b02c8f6d744df433669581de004469937c9a23
JayB202 <122235186+JayB202@users.noreply.github.com>
2024-06-08 19:15:53 +0900

    [Silver III] Title: 징검다리, Time: 68 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/11561. 징검다리/징검다리.py
+++ 백준/Silver/11561. 징검다리/징검다리.py
@@ -1,36 +1,34 @@
-import sys
-input = sys.stdin.readline
-
-def sumSequence(n):
-    return n * (n + 1) / 2
-
-def canReachN(n, maxJump):
-    # maxJump번 내에서 N번 징검다리에 도착 가능한지 확인
-    return sumSequence(maxJump) <= n
-    
-def binarySearch(n):
-    left, right = 1, n
-    answer = None
+def max_stepping_stones(N):
+    left, right = 1, int((2 * N) ** 0.5) + 1
+    max_steps = 1
 
     while left <= right:
         mid = (left + right) // 2
-        # print(n, mid, sumSequence(mid))
-        if canReachN(n, mid):
-            # n을 밟을 수 있으면
-            # 많은 점프가 가능한지 확인
-            answer = mid
+        sum_mid = mid * (mid + 1) // 2
+        
+        if sum_mid == N:
+            return mid
+        elif sum_mid < N:
+            max_steps = mid
             left = mid + 1
         else:
-            # n을 밟을 수 없으면
-            # 더 적은 점프면 가능한지 확인
             right = mid - 1
-            
-    return answer
 
-def main(nums):
-    for n in nums:
-        print(binarySearch(n))
+    return max_steps
 
-case = int(input())
-nums = [int(input()) for _ in range(case)]
-main(nums)
+def solve():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    T = int(data[0])
+    results = []
+
+    for i in range(1, T + 1):
+        N = int(data[i])
+        k = max_stepping_stones(N)
+        results.append(k)
+
+    for result in results:
+        print(result)
+
+solve()
