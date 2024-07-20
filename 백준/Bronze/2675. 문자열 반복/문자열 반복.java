3c031b67a7abb800888c6cfc614a8d1c13fd8e85
JayB202 <122235186+JayB202@users.noreply.github.com>
2024-07-20 17:23:37 +0900

    [Bronze II] Title: 문자열 반복, Time: 204 ms, Memory: 17536 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -4,19 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        // 첫 번째 줄에서 테스트 케이스의 개수를 읽습니다.
         int T = sc.nextInt();
-        sc.nextLine(); // 개행 문자를 소비하여 다음 입력을 준비합니다.
-
+        sc.nextLine(); 
+        
         for (int i = 0; i < T; i++) {
-            // 각 테스트 케이스를 읽습니다.
             int R = sc.nextInt();
             String S = sc.next();
             
-            // 새로운 문자열 P를 저장할 StringBuilder를 초기화합니다.
             StringBuilder P = new StringBuilder();
             
-            // 문자열 S의 각 문자를 R번 반복하여 P에 추가합니다.
+            // 문자열 S의 각 문자를 R번 반복하여 P에 추가
             for (int j = 0; j < S.length(); j++) {
                 char c = S.charAt(j);
                 for (int k = 0; k < R; k++) {
@@ -24,7 +21,6 @@ public class Main {
                 }
             }
             
-            // 결과 문자열 P를 출력합니다.
             System.out.println(P.toString());
         }
 
