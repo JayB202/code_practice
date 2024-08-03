50ee22c9d00ccf68748a56fccf85d2f9c97e20cb
JayB202 <122235186+JayB202@users.noreply.github.com>
2024-08-03 16:25:49 +0900

    [Gold IV] Title: 서울의 지하철, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/16166. 서울의 지하철/서울의 지하철.py
@@ -0,0 +1,36 @@
+from sys import stdin
+
+def bfs(start, cnt):
+	togo = []
+	for l in station[start]:
+		for y in line[l]:
+			if y == dest:
+				return cnt
+			if y not in visited:
+				if len(station[y]) > 1:
+					togo.append(y)
+				visited.append(y)
+	for x in togo:
+		return bfs(x, cnt + 1)
+
+
+n = int(stdin.readline().strip())
+visited = []
+line = {}
+station = {}
+for line_num in range(1, n + 1):
+	tmp = list(map(int, stdin.readline().split()))
+	line[line_num] = tmp[1:]
+	for a in tmp[1:]:
+		if a in station:
+			station[a].append(line_num)
+		else:
+			station[a] = [line_num]
+
+dest = int(stdin.readline().strip())
+visited.append(0)
+cnt = bfs(0, 0)
+if cnt == None:
+	print(-1)
+else:
+	print(cnt)
