5f430a9086db5884bd63d72a908e7158b675f295
JayB202 <122235186+JayB202@users.noreply.github.com>
2024-09-25 19:18:00 +0900

    [Silver IV] Title: 덱, Time: 56 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,13 +1,24 @@
+# 입력 : 첫째 줄에 주어지는 명령의 수 N, . 둘째 줄부터 N개의 줄에는 명령이 하나씩
+#
+# 로직 
+# push_front X: 정수 X를 덱의 앞에 넣는다.
+# push_back X: 정수 X를 덱의 뒤에 넣는다.
+# pop_front: 덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# pop_back: 덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# size: 덱에 들어있는 정수의 개수를 출력한다.
+# empty: 덱이 비어있으면 1을, 아니면 0을 출력한다.
+# front: 덱의 가장 앞에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# back: 덱의 가장 뒤에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+#
+#
+# 출력 : 한줄에 하나씩 명령 결과 출력 
+
 import sys
 from collections import deque
 
-# 입력을 빠르게 처리하기 위해 sys.stdin.readline 사용
 input = sys.stdin.readline
 
-# 덱 초기화
 dq = deque()
-
-# 명령의 수 입력
 n = int(input().strip())
 
 # 결과 저장 리스트
@@ -49,5 +60,4 @@ for _ in range(n):
         else:
             result.append(-1)
 
-# 결과 출력
 sys.stdout.write("\n".join(map(str, result)) + "\n")
