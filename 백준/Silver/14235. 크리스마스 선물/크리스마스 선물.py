ea92f525e40dc60252ddc081c4bfe2b15790231e
JayB202 <122235186+JayB202@users.noreply.github.com>
2024-09-28 19:43:23 +0900

    [Silver III] Title: 크리스마스 선물, Time: 268 ms, Memory: 40920 KB -BaekjoonHub

--- 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.py
+++ 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.py
@@ -1,17 +1,17 @@
 import heapq
 
 n = int(input().rstrip())
-max_heap = []  # max_heap을 루프 밖에서 초기화
+max_heap = []
 
 for _ in range(n):
     nums = list(map(int, input().rstrip().split()))
-    presentNum = nums.pop(0)  # 선물의 개수 변수로 담아줌 (첫 번째 요소)
-
+    presentNum = nums[0]  # 첫 번째 요소를 선물 개수로 사용
+    
     if presentNum == 0:  # 선물의 개수가 0일 때
-        if max_heap:  # 힙이 비어 있지 않은 경우
+        if max_heap:  # 힙이 비어있지 않은 경우
             print(-heapq.heappop(max_heap))  # 최대값 출력
         else:
-            print(-1)  # 힙이 비어 있을 경우 -1 출력
+            print(-1)  # 힙이 비어있을 경우 -1 출력
     else:
-        for num in nums:  # 선물 추가
+        for num in nums[1:]:  # 첫 번째 요소를 제외한 나머지를 선물 추가
             heapq.heappush(max_heap, -num)  # 음수로 추가하여 최대 힙처럼 사용
