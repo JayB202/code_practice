1aeee05b5401e2ffd274fe2c999db3b32867b427
JayB202 <122235186+JayB202@users.noreply.github.com>
2024-09-30 20:33:26 +0900

    [Silver II] Title: 좌표 압축, Time: 1500 ms, Memory: 252564 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,25 +1,36 @@
+# 입력 
+#    첫번째줄 : 좌표 개수 N 
+#    두번째줄 : 좌표 값 // 공백으로 구분
+
+# 로직 
+#    좌표 압축
+#        -> 중복을 제거하고, 작은 값부터 매핑해서 사용
+#        2 4 -10 4 -9   ->   2 3 0 3 1
+# 
+
+
+# 출력
+#    압축한 좌표 배열을 띄어쓰기해서 출력
+
+
+
+import sys
+
 def coordinate_compression(coords):
-    # 중복 제거 및 정렬
-    sorted_unique_coords = sorted(set(coords))
+    sorted_coords = sorted(set(coords))
     
-    # 좌표 값 -> 압축된 값 매핑 생성
-    coord_map = {coord: index for index, coord in enumerate(sorted_unique_coords)}
+    coord_map = {coord: index for index, coord in enumerate(sorted_coords)}
     
-    # 원래 좌표 값을 압축된 값으로 변환
     compressed_coords = [coord_map[coord] for coord in coords]
-    
     return compressed_coords
 
-# 입력 받기
-import sys
 input = sys.stdin.read
 data = input().split()
 
 N = int(data[0])
-coords = list(map(int, data[1:]))
+coords = list(map(int,data[1:]))
 
-# 좌표 압축 결과
-compressed_coords = coordinate_compression(coords)
+compressed_list = coordinate_compression(coords)
 
-# 결과 출력
-print(' '.join(map(str, compressed_coords)))
+print(' '.join(map(str, compressed_list)))
+
