4cf1c948db1d05a4cc6051163553193ea67a8821
JayB202 <122235186+JayB202@users.noreply.github.com>
2024-10-10 21:22:08 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,7 +1,39 @@
+def check_letter(array):
+    test = "".join(array)
+    if test == 'zero':
+        return '0'
+    elif test =='one':
+        return '1'
+    elif test == 'two':
+        return '2'
+    elif test =='three':
+        return '3'
+    elif test == 'four':
+        return '4'
+    elif test =='five':
+        return '5'
+    elif test =='six':
+        return '6'
+    elif test =='seven':
+        return '7'
+    elif test =='eight':
+        return '8'
+    elif test == 'nine':
+        return '9'
+    else:
+        return ''
+
 def solution(s):
-    answer = 0
-    num_dic ={"zero": "0", "one" : "1", "two" :"2", "three" : "3", "four" :"4", "five" :"5", "six" : "6", "seven" : "7","eight" : "8","nine" : "9"}
-    for i in num_dic :
-        s = s.replace(i, num_dic[i])
-    answer = int(s)
-    return answer
+
+    answer = []
+    temp =[]
+    for letter in s:
+        if letter in "0123456789":
+            answer.append(letter)
+        else:
+            temp.append(letter)
+            if check_letter(temp).isdigit():
+                answer.append(check_letter(temp))
+                temp=[]
+    final = int("".join(answer))
+    return final
