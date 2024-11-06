16ffa0a46393336864268f907851771e92fc7a9c
JayB202 <122235186+JayB202@users.noreply.github.com>
2024-11-06 23:51:11 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 122.32 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -0,0 +1,42 @@
+def solution(key, lock):
+    N = len(lock)
+    M = len(key)
+    
+    # 자물쇠의 홈 부분을 찾기 위해 lock을 확대하여 패딩을 추가
+    expanded_lock = [[0] * (N + 2 * (M - 1)) for _ in range(N + 2 * (M - 1))]
+    
+    # 확대된 lock의 가운데에 기존 lock을 배치
+    for i in range(N):
+        for j in range(N):
+            expanded_lock[i + M - 1][j + M - 1] = lock[i][j]
+    
+    # key를 회전시키는 함수
+    def rotate(key):
+        return [[key[M - 1 - j][i] for j in range(M)] for i in range(M)]
+    
+    # key가 lock에 맞는지 확인하는 함수
+    def can_open(x, y, key):
+        # 자물쇠의 홈 부분에 key를 맞추어 보며 확인
+        temp_lock = [row[:] for row in expanded_lock]
+        for i in range(M):
+            for j in range(M):
+                if key[i][j] == 1:
+                    if temp_lock[x + i][y + j] == 1:
+                        return False
+                    temp_lock[x + i][y + j] = 1
+        # 자물쇠의 모든 홈이 채워졌는지 확인
+        for i in range(N):
+            for j in range(N):
+                if temp_lock[i + M - 1][j + M - 1] == 0:
+                    return False
+        return True
+    
+    # key를 회전하면서 이동시키며 자물쇠를 열 수 있는지 확인
+    for rotation in range(4):
+        for x in range(N + M - 1):
+            for y in range(N + M - 1):
+                if can_open(x, y, key):
+                    return True
+        key = rotate(key)
+    
+    return False
