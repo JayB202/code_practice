92a24fb41f84a8d283510826708db6c4743e6c23
JayB202 <122235186+JayB202@users.noreply.github.com>
2025-07-17 06:03:05 +0900

    [Bronze II] Title: 문자열 분석, Time: 220 ms, Memory: 15456 KB -BaekjoonHub

---
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        while ((input = br.readLine()) != null) {
+            int lower = 0, upper = 0, digit = 0, space = 0;
+            for (char ch : input.toCharArray()) {
+                if (Character.isLowerCase(ch)) lower++;
+                else if (Character.isUpperCase(ch)) upper++;
+                else if (Character.isDigit(ch)) digit++;
+                else if (ch == ' ') space++;
+            }
+            System.out.printf("%d %d %d %d%n", lower, upper, digit, space);
+        }
+    }
+}
